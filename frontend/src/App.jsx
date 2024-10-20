292d5b0a51666638283eed04cfdb39b68896f8d2
kadirisunday <kadirisunday212@gmail.com>
2024-10-21 00:39:43 +0100

    Added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
-      HELLO Tailwind
+    <div className='mx-4 sm:mx[10%]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
