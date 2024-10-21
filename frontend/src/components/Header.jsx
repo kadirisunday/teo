592f940a631e64aa576dfc45eefaad27c8907162
kadirisunday <kadirisunday212@gmail.com>
2024-10-21 16:25:18 +0100

    cpmpleted the header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
     <div className='flex flex-col md:flex-row flx-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20'>
         {/* Left Side */}
         <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10w] md:mb-[-30px]'>
-            <p className='text-3xl md:text-4xl lg:text-5xl font-semibold leading-tight md:leading-tight'>Book Appointment<br/> With Trusted Doctors</p>
-            <div>
+            <p className='text-3xl md:text-4xl lg:text-5xl font-semibold leading-tight md:leading-tight'>Book Appointment<br className='hidden sm:block'/> With Trusted Doctors</p>
+            <div className='flex flex-col md:flex-row items-center gap-3 text-sm font-dark'>
                 <img src={assets.group_profiles} alt="" />
                 <p>Simply browse through extensive list of trusted doctors, <br/> schedule</p>
             </div>
-            <a href="">
+            <a className='flex items-center gap-2 bg-dark px-8 py-3 rounded-full' href="">
                 Book Appointment <img src={assets.arrow_icon} alt="" />
             </a>
         </div>
