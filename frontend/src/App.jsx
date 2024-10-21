776b0b3c1ed7b4b134a64963a29d895bfac90ed7
kadirisunday <kadirisunday212@gmail.com>
2024-10-21 10:42:51 +0100

    Added Navbar components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Doctor from './pages/Doctors'
+import Login from './pages/Login'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import MyAppointment from './pages/MyAppointment'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx[10%]'>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctor/>}/>
+        <Route path='/doctors:speciality' element={<Doctor/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/myappointment' element={<MyAppointment/>}/>
+        <Route path='/appointment' element={<Appointment/>}/>
+        <Route path='/myprofile' element={<MyProfile/>}/>
       </Routes>
     </div>
   )
---
+++ frontend/src/pages/Appointment.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Appointment = props => {
+  return (
+    <div>Appointment</div>
+  )
+}
+
+Appointment.propTypes = {}
+
+export default Appointment
