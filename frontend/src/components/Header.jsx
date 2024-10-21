9b5aa71c168ad1eb23d160a985e2e6d6e0c448ad
kadirisunday <kadirisunday212@gmail.com>
2024-10-21 18:10:19 +0100

    Created the Speciality components section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,20 +5,20 @@ const Header = () => {
   return (
     <div className='flex flex-col md:flex-row flx-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20'>
         {/* Left Side */}
-        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10w] md:mb-[-30px]'>
-            <p className='text-3xl md:text-4xl lg:text-5xl font-semibold leading-tight md:leading-tight'>Book Appointment<br className='hidden sm:block'/> With Trusted Doctors</p>
+        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
+            <p className='text-3xl text-white md:text-4xl lg:text-5xl font-semibold leading-tight md:leading-tight lg:leading-tight'>Book Appointment<br className='hidden sm:block'/> With Trusted Doctors</p>
             <div className='flex flex-col md:flex-row items-center gap-3 text-sm font-dark'>
                 <img src={assets.group_profiles} alt="" />
-                <p>Simply browse through extensive list of trusted doctors, <br/> schedule</p>
+                <p>Simply browse through extensive list of trusted doctors, <br className='hidden sm:block'/> schedule your appointment hassle-free</p>
             </div>
-            <a className='flex items-center gap-2 bg-dark px-8 py-3 rounded-full' href="">
+            <a className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300' href="#speciality">
                 Book Appointment <img src={assets.arrow_icon} alt="" />
             </a>
         </div>
 
         {/* Right Side */}
         <div className='md:w-1/2 relative'>
-        <img className='w-full md:absolute h-auto rounded-lg' src={assets.header_img} alt="" />
+        <img className='w-full md:absolute h-auto bottom-0 rounded-lg' src={assets.header_img} alt="" />
         </div>
     </div>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
                 </div>
                 : <button 
                 onClick={()=> navigate('/login')} 
-                className='bg-primary px-8 py-3 rounded-full font-light hidden md:block'>
+                className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>
                     Create Account
             </button>
             }
