0ddc6e640bffd5310a833f021e16b67e1b68c900
kadirisunday <kadirisunday212@gmail.com>
2024-10-22 15:06:25 +0100

    Added the login components

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             {/* Left section */}
             <div>
-                <img className='mb-5 w-40' src={assets.logo} alt="Eventsbag" />
+                <img className='mb-5 w-40' src={assets.logo} alt="Teo Logo" />
                 <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nesciunt sint a repudiandae laboriosam tenetur, iste sed, inventore accusamus, nemo recusandae fuga hic reiciendis expedita quaerat necessitatibus esse? Nulla, ex!</p>
             </div>
             {/* center section */}
@@ -25,7 +25,7 @@ const Footer = () => {
                 <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
                 <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>+1-212-456-7890</li>
-                    <li>admin@corehub.com.ng</li>
+                    <li>admin@teo.com</li>
                 </ul>
             </div>
         </div>
